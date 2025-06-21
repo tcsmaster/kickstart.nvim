b22b2eb7098484b1822c5b1af4e5431454feb014
Temesvári Csanád <csanad.t@gmail.com>
2025-06-21 13:59:40 +0200

    update cmdline

--- init.lua
+++ init.lua
@@ -784,65 +784,73 @@ require('lazy').setup({
       signature = { enabled = true },
     },
   },
-
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = 'horizon',
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          always_show_tabline = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+            refresh_time = 16, -- ~60fps
+            events = {
+              'WinEnter',
+              'BufEnter',
+              'BufWritePost',
+              'SessionLoadPost',
+              'FileChangedShellPost',
+              'VimResized',
+              'Filetype',
+              'CursorMoved',
+              'CursorMovedI',
+              'ModeChanged',
+            },
+          },
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {},
+      }
+    end,
+  },
   -- colorscheme
   {
     'AlexvZyl/nordic.nvim',
     lazy = false,
     priority = 1000,
     config = function()
-      require('nordic').setup {
-        -- This callback can be used to override the colors used in the base palette.
-        on_palette = function(palette) end,
-        -- This callback can be used to override the colors used in the extended palette.
-        after_palette = function(palette) end,
-        -- This callback can be used to override highlights before they are applied.
-        on_highlight = function(highlights, palette) end,
-        -- Enable bold keywords.
-        bold_keywords = false,
-        -- Enable italic comments.
-        italic_comments = true,
-        -- Enable editor background transparency.
-        transparent = {
-          -- Enable transparent background.
-          bg = false,
-          -- Enable transparent background for floating windows.
-          float = false,
-        },
-        -- Enable brighter float border.
-        bright_border = false,
-        -- Reduce the overall amount of blue in the theme (diverges from base Nord).
-        reduced_blue = true,
-        -- Swap the dark background with the normal one.
-        swap_backgrounds = false,
-        -- Cursorline options.  Also includes visual/selection.
-        cursorline = {
-          -- Bold font in cursorline.
-          bold = false,
-          -- Bold cursorline number.
-          bold_number = true,
-          -- Available styles: 'dark', 'light'.
-          theme = 'dark',
-          -- Blending the cursorline bg with the buffer bg.
-          blend = 0.85,
-        },
-        noice = {
-          -- Available styles: `classic`, `flat`.
-          style = 'classic',
-        },
-        telescope = {
-          -- Available styles: `classic`, `flat`.
-          style = 'flat',
-        },
-        leap = {
-          -- Dims the backdrop when using leap.
-          dim_backdrop = false,
-        },
-        ts_context = {
-          -- Enables dark background for treesitter-context window
-          dark_background = true,
-        },
-      }
+      require('nordic').load()
     end,
   },
   -- Highlight todo, notes, etc in comments

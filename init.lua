052bab0c8d169a1d2b99caa4c97c1c7e598f5130
Temesvári Csanád <csanad.t@gmail.com>
2025-07-03 21:32:55 +0200

    fugitive

--- init.lua
+++ init.lua
@@ -192,7 +192,7 @@ require('lazy').setup({
       },
     },
   },
-
+  { 'tpope/vim-fugitive', options = {} },
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
@@ -624,7 +624,8 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'stylua',
+        'ruff',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 

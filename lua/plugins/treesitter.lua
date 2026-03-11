cf44bc8a78bfb96c29b951fce0801e1b9315b0a0
tcsmaster <csanad.t@gmail.com>
2026-03-11 16:16:43 +0100

    treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,60 +1,61 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	lazy = false,
-	build = ':TSUpdate',
-	branch = 'main',
-	config = function()
-		---@param buf integer
-		---@param language string
-		local function treesitter_try_attach(buf, language)
-			-- check if parser exists and load it
-			if not vim.treesitter.language.add(language) then
-				return
-			end
-			-- enables syntax highlighting and other treesitter features
-			vim.treesitter.start(buf, language)
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
+  branch = 'main',
+  version = nil,
+  config = function()
+    ---@param buf integer
+    ---@param language string
+    local function treesitter_try_attach(buf, language)
+      -- check if parser exists and load it
+      if not vim.treesitter.language.add(language) then
+        return
+      end
+      -- enables syntax highlighting and other treesitter features
+      vim.treesitter.start(buf, language)
 
-			-- enables treesitter based folds
-			-- for more info on folds see `:help folds`
-			-- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      -- enables treesitter based folds
+      -- for more info on folds see `:help folds`
+      -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 
-			-- enables treesitter based indentation
-			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-		end
+      -- enables treesitter based indentation
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end
 
-		local available_parsers = require('nvim-treesitter').get_available()
-		vim.api.nvim_create_autocmd('FileType', {
-			callback = function(args)
-				local buf, filetype = args.buf, args.match
-				local language = vim.treesitter.language.get_lang(filetype)
-				if not language then
-					return
-				end
+    local available_parsers = require('nvim-treesitter').get_available()
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        local buf, filetype = args.buf, args.match
+        local language = vim.treesitter.language.get_lang(filetype)
+        if not language then
+          return
+        end
 
-				local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
+        local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
 
-				if vim.tbl_contains(installed_parsers, language) then
-					-- enable the parser if it is installed
-					treesitter_try_attach(buf, language)
-				elseif vim.tbl_contains(available_parsers, language) then
-					-- if a parser is available in `nvim-treesitter` enable it after ensuring it is installed
-					require('nvim-treesitter').install(language):await(function() end)
-				else
-					-- try to enable treesitter features in case the parser exists but is not available from `nvim-treesitter`
-					treesitter_try_attach(buf, language)
-				end
-			end,
-		})
-	end,
-	ensure_installed = { 'yaml', 'toml', 'python', 'bash', 'Dockerfile', 'rust' },
-	highlights = { enable = true },
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = 'gnn', -- set to `false` to disable one of the mappings
-			node_incremental = 'grn',
-			scope_incremental = 'grc',
-			node_decremental = 'grm',
-		},
-	},
+        if vim.tbl_contains(installed_parsers, language) then
+          -- enable the parser if it is installed
+          treesitter_try_attach(buf, language)
+        elseif vim.tbl_contains(available_parsers, language) then
+          -- if a parser is available in `nvim-treesitter` enable it after ensuring it is installed
+          require('nvim-treesitter').install(language):await(function() end)
+        else
+          -- try to enable treesitter features in case the parser exists but is not available from `nvim-treesitter`
+          treesitter_try_attach(buf, language)
+        end
+      end,
+    })
+  end,
+  ensure_installed = { 'yaml', 'toml', 'python', 'bash', 'Dockerfile', 'rust' },
+  highlights = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn', -- set to `false` to disable one of the mappings
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
+    },
+  },
 }
